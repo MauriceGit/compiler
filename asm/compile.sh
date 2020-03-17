cdb1c9f0e68b4f2ea538e7c6c9ef6553d35dddaa
mauricegit <maurice.tollmien@gmail.com>
2020-03-17 21:30:26 +0100

    remove asm again, now that the first tests work

--- asm/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-basename="${1%.*}"
-extension="${1##*.}"
-yasm -Worphan-labels -g dwarf2 -f elf64 "$basename.$extension"
-ld -g -o "$basename" "$basename.o"
-rm -f "$basename.o"
